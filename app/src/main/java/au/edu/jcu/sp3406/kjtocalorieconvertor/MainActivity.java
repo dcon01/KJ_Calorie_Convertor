54895c97a8f431188b1ccc4179574b5abc642fa2
Danny <danny.connolly@my.jcu.edu.au>
2021-03-26 19:42:25 +1100

    commit for review

--- app/src/main/java/au/edu/jcu/sp3406/kjtocalorieconvertor/MainActivity.java
+++ app/src/main/java/au/edu/jcu/sp3406/kjtocalorieconvertor/MainActivity.java
@@ -1,5 +1,6 @@
 package au.edu.jcu.sp3406.kjtocalorieconvertor;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,20 +8,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    public final  double calories = 0.239005736;
-    public double conversion = 0;
+    private boolean multiply = true; // sets to default convert to KC from KJ
     private String userInput = "";
+    final double unit = 4.2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        conversion = calories;
     }
 
-    public void buttonClicked(View view){
+    public void buttonClicked(View view) { // when a number button is clicked collect text and add it to view
         Button button = (Button) view;
         String buttonNumber = button.getText().toString();
         userInput = userInput + buttonNumber;
@@ -28,22 +29,42 @@ public class MainActivity extends AppCompatActivity {
         textView.setText(userInput);
     }
 
-    public void settingsClicked(View view) {
+    public void settingsClicked(View view) { // call settingsActivity
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivityForResult(intent, SettingsActivity.SETTINGS_REQUEST);
     }
 
-    public void buttonClear(View view){
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) { // collect data passed from settings activity
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == SettingsActivity.SETTINGS_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                if (data != null) {
+                    multiply = data.getBooleanExtra("multiply", multiply);
+                }
+            }
+        }
+    }
+
+    public void buttonClear(View view) { // clear view
         TextView textView = findViewById(R.id.text);
         textView.setText("");
         userInput = "";
     }
 
-    public void buttonConvert(View view){
+    //err - convert button will only work once - then I need to clear it to make it work again?
+    public void buttonConvert(View view) { //takes user input and will either convert to KC or KG depending on user setting option - default is KC
         TextView textView = findViewById(R.id.text);
         int userInputInt = Integer.parseInt(userInput);
-        int result = (int) (userInputInt * conversion);
+        int result;
+        if (multiply) {
+            result = (int) (userInputInt * unit);
+        } else {
+            result = (int) (userInputInt / unit);
+        }
         String showResult = "" + result;
         textView.setText(showResult);
+        Toast.makeText(this, "Converted!", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/au/edu/jcu/sp3406/kjtocalorieconvertor/SettingsActivity.java
+++ app/src/main/java/au/edu/jcu/sp3406/kjtocalorieconvertor/SettingsActivity.java
@@ -4,51 +4,44 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.LinearLayoutCompat;
 
 
 public class SettingsActivity extends AppCompatActivity {
 
     public static final int SETTINGS_REQUEST = 1;
-
+    public boolean multiply;
+    private TextView text;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {//create text view on create
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
-
-//        if (savedInstanceState == null) {
-//            getSupportFragmentManager()
-//                    .beginTransaction()
-//                    .replace(R.id.settings, new SettingsFragment())
-//                    .commit();
-        }
-//
-//    public static class SettingsFragment extends PreferenceFragmentCompat {
-//        @Override
-//        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-//            setPreferencesFromResource(R.xml.root_preferences, rootKey);
-//        }
-//    }
-
-    public void buttonCalories(View view){
-
+        text = findViewById(R.id.text);
     }
 
-    public void buttonKilojules(View view){
-
+    @SuppressLint("SetTextI18n") //Not sure why I need to do this?
+    public void buttonCalories(View view){//set to convert KC to KJ
+        multiply = true;
+        text.setText("Calories");
+        //text.setText(cal.toString()); test to see if this works with out the SuppressLint("SetTextI18n")
     }
 
-    public void doneClicked(View view) {
+    @SuppressLint("SetTextI18n") //Not sure why I need to do this?
+    public void buttonKilojoules(View view){//set to convert KJ to KC
+        multiply = false;
+        text.setText("KiloJoules");
+    }
 
+    public void doneClicked(View view) { //pass on KC or KJ selection to main activity
         Intent intent = new Intent();
-        //intent.putExtra("conversion", conversion);
+        intent.putExtra("multiply", multiply);
         setResult(RESULT_OK, intent);
         finish();
+        Toast.makeText(this, "Saved!", Toast.LENGTH_SHORT).show();
     }
 
     @Override
